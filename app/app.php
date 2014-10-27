f300cbdb9e8b453706e00d5b424f254582d29d6c
Maeva Dubourg <maeva.dbg@gmail.com>
2014-10-27 18:20:29 +0100

    Config app dependencies

--- app/app.php
+++ app/app.php
@@ -9,14 +9,21 @@ use Silex\Provider\MonologServiceProvider;
 $app = new Application();
 
 $app->register(new ServiceControllerServiceProvider());
-$app->register(new TwigServiceProvider());
 $app->register(new UrlGeneratorServiceProvider());
 $app->register(new SessionServiceProvider());
 
-$app['twig.path'] = array(__DIR__.'/../src/view');
-$app['twig.options'] = array('cache' => __DIR__.'/cache/twig');
+//add Twig template engine
+$app->register(new TwigServiceProvider());
+$app['twig.path'] = array(__DIR__.'/../src/view'); //template dir
+$app['twig.options'] = array('cache' => __DIR__.'/cache/twig'); //cache dir
+//add extensions for Twig
+$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
+    //add filter for localization of DateTime objects
+    $twig->addExtension(new Twig_Extensions_Extension_Intl($app));
+    return $twig;
+}));
 
-//enable log for requests and errors
+//add log for requests and errors
 $app->register(new MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__.'/logs/silex.log',
 ));
