aa4240a3b0216fd0205d4b84652a9aceb1fc9b3c
Maeva Dubourg <maeva.dbg@gmail.com>
2014-10-27 12:03:24 +0100

    Config app depandencies

--- app/app.php
+++ app/app.php
@@ -4,7 +4,6 @@ use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\Provider\SessionServiceProvider;
-use Symfony\Component\HttpFoundation\Response;
 use Silex\Provider\MonologServiceProvider;
 
 $app = new Application();
@@ -14,12 +13,12 @@ $app->register(new TwigServiceProvider());
 $app->register(new UrlGeneratorServiceProvider());
 $app->register(new SessionServiceProvider());
 
-$app['twig.path'] = array(__DIR__.'/../../src/view');
-$app['twig.options'] = array('cache' => __DIR__.'/../cache/twig');
+$app['twig.path'] = array(__DIR__.'/../src/view');
+$app['twig.options'] = array('cache' => __DIR__.'/cache/twig');
 
 //enable log for requests and errors
 $app->register(new MonologServiceProvider(), array(
-    'monolog.logfile' => __DIR__.'/../logs/silex_dev.log',
+    'monolog.logfile' => __DIR__.'/logs/silex.log',
 ));
 
 return $app;
