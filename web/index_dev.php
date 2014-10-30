4da965be8a6b8942136bbb1ac40c7509fdecd24d
MavCite <mdubourg@cite-musique.fr>
2014-10-30 17:01:32 +0100

    Fix autoload require

--- web/index_dev.php
+++ web/index_dev.php
@@ -1,10 +1,10 @@
 <?php
 ini_set('display_errors', 1);
 
+require_once __DIR__.'/../vendor/autoload.php';
+
 use Symfony\Component\Debug\Debug;
 Debug::enable();
-
-require_once __DIR__.'/../vendor/autoload.php';
 
 $app = require __DIR__.'/../app/app.php';
 require __DIR__.'/../app/config/dev.php';
