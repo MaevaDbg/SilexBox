e6fbe562b7f5faaf8cc153462cd9d7876dae017d
MavCite <mdubourg@cite-musique.fr>
2014-10-31 14:32:58 +0100

    add 404 page

--- src/controller.php
+++ src/controller.php
@@ -36,19 +36,14 @@ $app->match('/email-subscription', function (Request $request) use ($app) {
 /*-----  End of email subscription  ------*/
 
 
+/*==============================
+=            ERREUR            =
+==============================*/
 $app->error(function (\Exception $e, $code) use ($app) {
     if ($app['debug']) {
         return;
     }
-
-    // 404.html, or 40x.html, or 4xx.html, or error.html
-    /*$templates = array(
-        'errors/'.$code.'.html',
-        'errors/'.substr($code, 0, 2).'x.html',
-        'errors/'.substr($code, 0, 1).'xx.html',
-        'errors/default.html',
-    );
-    return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);*/
-    return new Response('Erreur');
+    return $app['twig']->render('404.html.twig');
 });
+
 ?>
